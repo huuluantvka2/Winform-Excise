c0e6358549440fa88d02a7ea9b58bb642f04bfbc
luan.le@aegona.com <luan.le@aegona.com>
2023-03-27 16:16:04 +0700

    Initial Commit

---
+++ ExerciseWinform/Signin.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace ExerciseWinform
+{
+    partial class Signin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbUsername = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label1.Location = new System.Drawing.Point(376, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LOGIN";
+            // 
+            // lbUsername
+            // 
+            this.lbUsername.AutoSize = true;
+            this.lbUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUsername.Location = new System.Drawing.Point(117, 113);
+            this.lbUsername.Name = "lbUsername";
+            this.lbUsername.Size = new System.Drawing.Size(129, 29);
+            this.lbUsername.TabIndex = 1;
+            this.lbUsername.Text = "UserName";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(275, 120);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(563, 22);
+            this.txtUserName.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(275, 178);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(563, 22);
+            this.txtPassword.TabIndex = 4;
+            this.txtPassword.UseSystemPasswordChar = true;
+            this.txtPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPassword_KeyPress);
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPassword.Location = new System.Drawing.Point(117, 171);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(120, 29);
+            this.lbPassword.TabIndex = 3;
+            this.lbPassword.Text = "Password";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLogin.Location = new System.Drawing.Point(338, 248);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(121, 59);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Signin";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.LightCoral;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnReset.Location = new System.Drawing.Point(530, 248);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(121, 59);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Signin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Moccasin;
+            this.ClientSize = new System.Drawing.Size(956, 753);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.lbUsername);
+            this.Controls.Add(this.label1);
+            this.Name = "Signin";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Signin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbUsername;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ ExerciseWinform/Signin.cs
@@ -0,0 +1,61 @@
+﻿using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ExerciseWinform
+{
+    public partial class Signin : Form
+    {
+        SqlConnection cnn;
+        public Signin()
+        {
+            InitializeComponent();
+            string connetionString;
+            connetionString = @"Data Source=210.2.99.122,1444;Initial Catalog=LuanTest;User ID=sa;Password=Xx12345689xX";
+            cnn = new SqlConnection(connetionString);
+            cnn.Open();
+        }
+
+        private void Signin_Load(object sender, System.EventArgs e)
+        {
+            this.Name = "Login";
+        }
+
+        private void btnLogin_Click(object sender, System.EventArgs e)
+        {
+            Login();
+        }
+
+        private void txtPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                Login();
+            }
+        }
+        private void Login()
+        {
+            using (SqlCommand cmd = new SqlCommand($"SELECT TOP 1 Id FROM Employees WHERE Email='{txtUserName.Text}' AND Password='{txtPassword.Text}';", this.cnn))
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+
+                    if(reader.HasRows)
+                    {
+                        MessageBox.Show("Login success");
+                    }
+                    else MessageBox.Show("Username or password incorrect");
+                }
+            }
+        }
+        private void Reset()
+        {
+            txtUserName.Text = "";
+            txtPassword.Text = "";
+        }
+
+        private void btnReset_Click(object sender, System.EventArgs e)
+        {
+            Reset();
+        }
+    }
+}
